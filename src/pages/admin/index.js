d3b11f06b8717baec485de77138cbeb0efc047cb
Varpenikhil0007 <varpenikhil0007@gmail.com>
2025-01-15 22:58:12 +0530

    commit

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -76,7 +76,7 @@ const Admin = () => {
               type="text"
               id="image"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="https://google.com/tanmay.png"
+              placeholder=""
               required
               value={image}
               onChange={(e) => setImage(e.target.value)}
